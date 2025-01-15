6e7b53f28d318741ac3e1cd42b28bd3e7c9e8c43
siugell <chlworud7722@naver.com>
2025-01-15 18:16:39 +0900

    캐릭터 완성

--- My project/Assets/code/PlayerMove.cs
+++ My project/Assets/code/PlayerMove.cs
@@ -16,6 +16,10 @@ public class PlayerMove : MonoBehaviour
     private bool isJumping = false; 
     private bool canWalk = true;
     private bool isDown = false; 
+    private bool isFall = false;
+    private bool isStun = false;
+    private float fallTimer = 0f; // 하강 시간 누적용
+    public float fallThreshold = 3f; // 몇 초 이상이면 isFall로 판정할지
 
     private void Awake() {
         
@@ -28,7 +32,7 @@ public class PlayerMove : MonoBehaviour
     void Update(){
         // 버튼에서 손을 떄는 등의 단발적인 키보드 입력은 FixedUpdate보다 Update에 쓰는게 키보드 입력이 누락될 확률이 낮아짐
 
-        if(canWalk){
+        if(canWalk && !isStun){
         //Stop speed 
         if(Input.GetButtonUp("Horizontal")){ // 버튼에서 손을 때는 경우 
             // normalized : 벡터 크기를 1로 만든 상태 (단위벡터 : 크기가 1인 벡터)
@@ -44,24 +48,28 @@ public class PlayerMove : MonoBehaviour
         //Animation
         if( Mathf.Abs(rigid.velocity.x) < 0.2) //속도가 0 == 멈춤 
             animator.SetBool("isWalking",false); //isWalking 변수 : false 
-        else// 이동중 
-            animator.SetBool("isWalking",true);
+        else// 이동중
+            if(!isJumping) 
+                animator.SetBool("isWalking",true);
 
         // 점프 버튼 입력 처리 (기본적으로 스페이스바 사용)
-        if (Input.GetKeyDown(KeyCode.Space) && !(isJumping))
+        if (Input.GetKeyDown(KeyCode.Space) && !(isJumping) && !(isStun))
         {
             StartCharging();
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && !(isStun))
         {
             ChargeJump();
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space) && !(isStun))
         {
             PerformJump();
         }
+
+        Debug.Log(canWalk);
+        //Debug.Log(rigid.velocity.x);
     }
 
     // Update is called once per frame
@@ -69,10 +77,10 @@ public class PlayerMove : MonoBehaviour
     {
         float h = canWalk ? Input.GetAxisRaw("Horizontal") : 0f;
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        
 
         if(rigid.velocity.x > maxSpeed)  //오른쪽으로 이동 (+) , 최대 속력을 넘으면 
             rigid.velocity= new Vector2(maxSpeed, rigid.velocity.y); //해당 오브젝트의 속력은 maxSpeed 
-        
         else if(rigid.velocity.x < maxSpeed*(-1)) // 왼쪽으로 이동 (-) 
             rigid.velocity =  new Vector2(maxSpeed*(-1), rigid.velocity.y); //y값은 점프의 영향이므로 0으로 제한을 두면 안됨 
 
@@ -87,9 +95,21 @@ public class PlayerMove : MonoBehaviour
         if(rigid.velocity.y < 0){ // 뛰어올랐다가 아래로 떨어질 때만 빔을 쏨 
             if(rayHit.collider != null){ //빔을 맞은 오브젝트가 있을때  -> 맞지않으면 collider도 생성되지않음 
                 if(rayHit.distance < 0.5f) 
+                {
                     animator.SetBool("isJumping",false); //거리가 0.5보다 작아지면 변경
                     isJumping = false;
-                    canWalk = true; //점프 후 착지할때 canWalk = true
+                    if (isFall)
+                    {
+                        // 추락 종료 → 기절 시작
+                        StartCoroutine(StunRoutine(2f)); // 2초 동안 기절
+                        isFall = false; // isFall도 해제
+                    }
+
+                    if(!isStun)
+                    {
+                        canWalk = true;
+                    }
+                }
             }
         }
 
@@ -102,12 +122,7 @@ public class PlayerMove : MonoBehaviour
             {
                 // 이제 막 하강으로 전환된 경우
                 isDown = true;
-                animator.SetTrigger("Fall");
-            }
-            else if (rigid.velocity.y > 0 && isDown)
-            {
-                // 다시 상승 중으로 돌아갔으면(이 상황은 드물지만) isDown 해제
-                isDown = false;
+                animator.SetTrigger("isDown");
             }
         }
         else
@@ -115,6 +130,46 @@ public class PlayerMove : MonoBehaviour
             // 바닥에 있거나 점프 전 상태면 하강이 아님
             isDown = false;
         }
+
+        // ---- (추가) 하강 시간(fallTimer) 로직 ----
+        if (isDown)
+        {
+            // 하강 중이면 하강 시간 누적
+            fallTimer += Time.deltaTime; // 혹은 Time.fixedDeltaTime
+
+            if (fallTimer >= fallThreshold && !isFall)
+            {
+                // 특정 시간 이상 하강 상태에 돌입한 경우 추락 상태로 변경
+                isFall = true;
+                animator.SetBool("isFall", true);
+            }
+        }
+        else
+        {
+            // 하강이 종료되거나 아직 하강이 아닌 경우
+            fallTimer = 0f;
+            isFall = false;
+            isDown = false;
+            animator.SetBool("isFall", false);
+        }
+    }
+
+    // 기절(Stun) 상태를 2초 동안 유지하는 코루틴
+    IEnumerator StunRoutine(float stunDuration)
+    {
+        Debug.Log("stuned!");
+        isStun = true;   // 기절 상태
+        canWalk = false; // 이동 불가
+        rigid.velocity = new Vector2(0f, rigid.velocity.y);
+        
+        animator.SetBool("isStun", true);
+
+        yield return new WaitForSeconds(stunDuration);
+
+        // 2초 뒤 기절 해제
+        isStun = false;
+        animator.SetBool("isStun", false);
+        canWalk = true;
     }
 
     void StartCharging()
@@ -139,6 +194,18 @@ public class PlayerMove : MonoBehaviour
     {
         if (isCharging)
         {
+            float horizontalForce = 5f;
+            float h = Input.GetAxisRaw("Horizontal");
+            if (h < 0)
+            {
+            // 왼쪽으로 힘
+            rigid.AddForce(Vector2.left * horizontalForce, ForceMode2D.Impulse);
+            }
+            else if (h > 0)
+            {
+            // 오른쪽으로 힘
+            rigid.AddForce(Vector2.right * horizontalForce, ForceMode2D.Impulse);
+            }
             // 차징 비율 계산 (0~1 사이 값)
             float chargeRatio = currentChargeTime / chargeTime;
 
@@ -154,8 +221,6 @@ public class PlayerMove : MonoBehaviour
             animator.SetBool("isCharging", false);
 
             isJumping = true; // 점프 상태로 설정
-
-            canWalk = true;
         }
     }
 }
