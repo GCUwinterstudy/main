cfec048993f3956ae22fb0763530e1772f093173
9September <ydking0911@gmail.com>
2025-02-12 15:08:56 +0900

    Update PlayerSpawner.cs

--- My project/Assets/code/PlayerSpawner.cs
+++ My project/Assets/code/PlayerSpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 using UnityEngine;
 
@@ -8,45 +9,55 @@ public class PlayerSpawner : MonoBehaviourPunCallbacks
 {
     public Vector3 spawnPosition = new Vector3(0f, 0f, 0f);
     public float nameYOffset = 0f;
+    private bool spawned = false;
 
     void Start() 
     {
-        GameObject character = null;
-
-        // 멀티플레이일 때는 PhotonNetwork를 이용하여 캐릭터 생성
-        if (PhotonNetwork.InRoom)
+        // 만약 PhotonNetwork에 연결되어 있지 않다면(즉, 싱글플레이라면)
+        // 오프라인 모드를 활성화하고 임시 방을 생성하여 PhotonNetwork.Instantiate가 사용되도록 합니다.
+        if (!PhotonNetwork.InRoom)
         {
-            character = PhotonNetwork.Instantiate("Character", spawnPosition, Quaternion.identity);
+            PhotonNetwork.OfflineMode = true;
+            PhotonNetwork.CreateRoom("OfflineRoom", new RoomOptions { MaxPlayers = 1 });
+            Debug.Log("OfflineMode 활성화 및 방 생성 (싱글플레이)");
         }
-        // 싱글플레이일 경우, Resources 폴더에 있는 "Character" 프리팹을 로드하여 생성
         else
         {
-            Object prefab = Resources.Load("Character");
-            if (prefab != null)
-            {
-                character = Instantiate(prefab, spawnPosition, Quaternion.identity) as GameObject;
-            }
-            else
-            {
-                Debug.LogError("Character 프리팹을 Resources 폴더에서 찾을 수 없습니다.");
-            }
+            SpawnCharacter();
         }
+    }
 
-        // 캐릭터가 정상적으로 생성되었다면, 본인이 조종하는 캐릭터에 이름 표시 추가
+    // 방에 가입되었을 때 호출됩니다.
+    public override void OnJoinedRoom()
+    {
+        if (!spawned)
+        {
+            SpawnCharacter();
+        }
+    }
+
+    void SpawnCharacter() 
+    {
+        spawned = true;
+        // 온라인이든 오프라인이든 항상 PhotonNetwork.Instantiate를 사용하여 캐릭터 생성
+        GameObject character = PhotonNetwork.Instantiate("Character", spawnPosition, Quaternion.identity);
+
         if (character != null)
         {
-            // 멀티플레이의 경우 PhotonView.IsMine, 싱글플레이의 경우에는 무조건 본인
+            // PhotonNetwork.InRoom가 true인 경우, 멀티플레이라면 PhotonView의 소유권 체크,
+            // 오프라인 모드(싱글플레이)라면 무조건 본인으로 간주
             bool isMine = PhotonNetwork.InRoom ? character.GetComponent<PhotonView>().IsMine : true;
             if (isMine)
             {
-                // 이름 표시용 게임오브젝트 생성 및 부모 설정
+                // 캐릭터 이름 표시용 게임오브젝트 생성 및 자식으로 설정
                 GameObject nameIndicator = new GameObject("MyCharacterName");
                 nameIndicator.transform.SetParent(character.transform, false);
                 nameIndicator.transform.localPosition = new Vector3(0f, nameYOffset, 0f);
 
-                // TextMeshPro 컴포넌트 추가 후 이름 설정
+                // TextMeshPro 컴포넌트 추가 및 이름 설정
                 TextMeshPro textMesh = nameIndicator.AddComponent<TextMeshPro>();
-                string playerName = PhotonNetwork.InRoom ? PhotonNetwork.LocalPlayer.NickName : "Player";
+                // 멀티플레이라면 PhotonNetwork.LocalPlayer.NickName, 싱글플레이라면 기본 문자열 사용
+                string playerName = PhotonNetwork.InRoom ? PhotonNetwork.LocalPlayer.NickName : "";
                 textMesh.text = playerName;
                 textMesh.fontSize = 3f;
                 textMesh.alignment = TextAlignmentOptions.Bottom;
