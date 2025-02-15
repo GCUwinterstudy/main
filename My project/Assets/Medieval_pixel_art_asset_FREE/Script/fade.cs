d9c02bc169e6157d11b8db9cf702cc24e49334c2
park seonjun <qkrtjswns@gachon.ac.kr>
2025-02-15 18:32:26 +0900

    사라지는발판완료
    움직이는 발판에서 떨어졌을 때 플레이어 자동으로 움직이는 오류 있음

--- My project/Assets/Medieval_pixel_art_asset_FREE/Script/fade.cs
+++ My project/Assets/Medieval_pixel_art_asset_FREE/Script/fade.cs
@@ -1,47 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Tilemaps;
 using UnityEngine;
 
 public class fade : MonoBehaviour
 {
     public float respawnTime = 2.0f; // 다시 나타나기까지의 시간
-    public float waitTime = 1.0f; //사라지기 전까지 시간
-    private SpriteRenderer spriteRenderer;
+    public float waitTime = 10.0f; //사라지기 전까지 시간
+    private TilemapRenderer tilemapRenderer;
     private Collider2D platformCollider;
     GameObject player;
-    private bool isCoroutineRunning = false;
+    private bool isCoroutineRunning;
 
     
 
     void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        tilemapRenderer = GetComponent<TilemapRenderer>();
         platformCollider = GetComponent<Collider2D>();
     }
- 
+    private void Start()
+    {
+        isCoroutineRunning = false;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !isCoroutineRunning)
         {
-            Debug.Log("Trigger");
-            player = collision.gameObject;
             StartCoroutine(DisappearAndRespawn());
         }
     }
 
     IEnumerator DisappearAndRespawn()
     {
-        Debug.Log("Coroutine");
         isCoroutineRunning = true; //코루틴 활성화
 
-        yield return new WaitForSeconds(respawnTime);
+        yield return new WaitForSeconds(waitTime);
 
-        spriteRenderer.enabled = false; //발판 안보이게
+        tilemapRenderer.enabled = false; //발판 안보이게
         platformCollider.enabled = false; //발판 비활성화
 
         yield return new WaitForSeconds(respawnTime); //respawnTime 만큼 대기
 
-        spriteRenderer.enabled = true; //발판 보이게
+        tilemapRenderer.enabled = true; //발판 보이게
         platformCollider.enabled = true; // 발판 활성화
 
         isCoroutineRunning = false; //코루틴 비활성화
